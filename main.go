0bf508179c05ebb8c2c98cbfa16241265b55a8b2
Vishal Shetagar <vishal.shetegar1@gmail.com>
2026-02-20 15:20:28 +0530

    feat: raven v2 - fetch and extract links concurrently

--- main.go
+++ main.go
@@ -2,27 +2,57 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"regexp"
 	"sync"
 )
 
 func worker(id int, jobs <-chan string, wg *sync.WaitGroup) {
 	for url := range jobs {
-		fmt.Println("Worker", id, "feching:", url)
+		func() {
+			defer wg.Done()
 
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Println("Worker", id, "error:", err)
-			wg.Done()
-			continue
-		}
+			fmt.Println("Worker", id, "fetching:", url)
 
-		fmt.Println("Worker", id, "status:", resp.Status)
-		resp.Body.Close()
+			client := &http.Client{}
+			req, _ := http.NewRequest("GET", url, nil)
+			req.Header.Set("User-Agent", "RAVEN-Bot/1.0")
 
-		wg.Done()
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Worker", id, "error:", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			bodyBytes, err := io.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println("Worker", id, "read error:", err)
+				return
+			}
+
+			links := extractLinks(string(bodyBytes))
+
+			fmt.Println("Worker", id, "found links:")
+			for _, link := range links {
+				fmt.Println(" ", link)
+			}
+
+		}()
 	}
+}
+
+func extractLinks(body string) []string {
+	re := regexp.MustCompile(`href="(http[s]?://[^"]+)"`)
+	matches := re.FindAllStringSubmatch(body, -1)
+
+	var links []string
+	for _, match := range matches {
+		links = append(links, match[1])
+	}
+	return links
 }
 
 func main() {
